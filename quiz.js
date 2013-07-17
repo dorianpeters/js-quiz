352a811a76a75fafe40bb04fb24bad5467166511
zenstunna <dpeters08@gmail.com>
2013-07-16 21:50:16 -0700

    Initial answers working

--- quiz.js
+++ quiz.js
@@ -6,10 +6,10 @@ var questions = [{
 
 $(document).ready(function () {
   $('#question').text(questions[0].question);
-  $('#ans0').text(' text');
-  $('#ans1').text(' text');
-  $('#ans2').text(' text');
-  $('#ans3').text(' text');
+  $('#ans0').text(questions[0].choices[0]);
+  $('#ans1').text(questions[0].choices[1]);
+  $('#ans2').text(questions[0].choices[2]);
+  $('#ans3').text(questions[0].choices[3]);
 
   $('button').on('click', function () {
     // run this function on click. Will use to advance question and talley score.
