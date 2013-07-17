beb5b3fdf53d8c19f1aa7b479c7dcc8334539c97
zenstunna <dpeters08@gmail.com>
2013-07-16 21:01:04 -0700

    Added answers variable, renumbered answer text

--- quiz.js
+++ quiz.js
@@ -1,3 +1,9 @@
+var questions = [{ 
+    question: "What is my name?",
+    choices: ["Dorian", "David", "Bill", "George"],
+    correctAnswer: 0
+}];
+
 $(document).ready(function() {
   $('button').on('click', function() {
   // run this function on click
