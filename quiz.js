11e4338f6640835cdd3f89674772954cfad865e6
Dorian Peters <dpeters08@gmail.com>
2013-12-25 17:11:37 -0800

    Remove hardcoded answers from html, adapting js

--- quiz.js
+++ quiz.js
@@ -10,29 +10,36 @@ var questions = [
     correctAnswer: 2  
   },
   {
-  question: "What is my favorite color?",
-  choices: ["blue", "green", "orange", "red"],
-  correctAnswer: 0  
+    question: "What is my favorite color?",
+    choices: ["blue", "green", "orange", "red"],
+    correctAnswer: 0  
   },
   {
-  question: "How tall am I?",
-  choices: ["5-10", "5-11", "6-0", "6-1"],
-  correctAnswer: 2  
+    question: "How tall am I?",
+    choices: ["5-10", "5-11", "6-0", "6-1"],
+    correctAnswer: 2  
   },
   {
-  question: "Which OS is my favorite?",
-  choices: ["Window 7", "Windows 8", "Linux", "Mac OS"],
-  correctAnswer: 3  
+    question: "Which OS is my favorite?",
+    choices: ["Window 7", "Windows 8", "Linux", "Mac OS"],
+    correctAnswer: 3  
   },
 ];
 
-  $(document).ready(function () {
-    var maxQuestions = questions.length;
-    var numRight = 0;
-    var counter = 0;
+$(document).ready(function () {
+  var maxQuestions = questions.length;
+  var numRight = 0;
+  var counter = 0;
+
+  if (counter >= maxQuestions) {
+      document.getElementById('results').innerHTML=
+        "Quiz is over. You got ", numRight, " out of ", maxQuestions);
+      return;
+  }
 
     // display first question
     $('#question').text(questions[counter].question);
+
     $('#ans0').text(questions[counter].choices[0]);
     $('#ans1').text(questions[counter].choices[1]);
     $('#ans2').text(questions[counter].choices[2]);
@@ -47,10 +54,7 @@ var questions = [
 
       counter++;
 
-      if (counter >= maxQuestions) {
-        document.write("Quiz is over. You got ", numRight, " out of ", maxQuestions);
-        return;
-      }
+
 
       // Show next question
       $('#question').text(questions[counter].question);
